1824b84d6a49c9fdf62e95e108ef3b9fd740e03a
agobel <115437274+agobel@users.noreply.github.com>
2024-06-26 16:23:01 -0500

    Update A04.java

--- Assignments/Assignment_04/A04.java
+++ Assignments/Assignment_04/A04.java
@@ -4,5 +4,25 @@ public class A04 {
         printEvenCharacters("Springfield"); // Expect: S r n f e d 
     } // method main
 
+    static void printOddCharacters(String string) {
+        for(int position = 0; position < string.length(); position = position +1) {
+            if (position %2 == 0) {
+                continue;
+            }
+            char current = string.charAt(position);
+            System.out.printf("\nAt position %2d the character is %s", position, current);
+            }
+        }
+
+    static void printEvenCharacters(String string) {
+         for(int position = 0; position < string.length(); position = position +1) {
+            if (position %2 == 1) {
+                continue;
+            }
+            char current = string.charAt(position);
+            System.out.printf("\nAt position %2d the character is %s", position, current);
+            }
+        }
 
 }
+
