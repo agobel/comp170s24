471f57de08be6409986470212e0295b68214accb
agobel <115437274+agobel@users.noreply.github.com>
2024-07-17 16:49:36 -0500

    Update A07.java

--- Assignments/Assignment_07/A07.java
+++ Assignments/Assignment_07/A07.java
@@ -18,5 +18,41 @@ public class A07 {
                 "\nI also found %d more words in the book with the same length.\n\n",
                 asLongAsLongest - 1);
     } // method main
+
+    public static String findLongestWord(String fileName) {
+        String longestWord = ""; // initialize longestWord to be empty
+        try (Scanner scanner = new Scanner(new File(fileName))) { // create a scanner to read the file
+            while (scanner.hasNext()){
+                String word = scanner.next(); // read the next word
+                if (word.length() > longestWord.legth()) {
+                    longestWord = word; // update longestWord if the current word is longer than previous
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + fileName); // print an error message is something is not found 
+        }
+        return longestWord;
+    } // method to find the longest word
+    
+    public static int countWithLength(int length, String fileName) {
+        int count = 0; // initialize count to zero
+        try (Scanner scanner = new Scanner(new File(fileName))) { // create a scanner to read the file
+            while (scanner.hasNext()) {
+                String word = scanner.next(); // read the next word from the file
+                if (word.length() == length) {
+                    count++; // increase count if the word matches the specified length
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + fileName); // print an error message is something is not found
+        }
+        return count; // return the count of words with the specified length
+    } // method to count words of a specific length
+
+    public static int countWordsEqualToLongest(String fileName) {
+        String longestWord = findLongestWord(fileName); // find the longest word
+        int longesLength = longestWord.length(); // get the length of the longest word
+        return countWithLength(longestLength, fileName); // count the words that match the longest word
+    } // method to count words that are as long as the longest word
     
 } // class A07
